cc8ea8c7722bd3750959fca2a7196f3115c71115
Marius Bejan <103320303@student.swin.edu.au>
2021-09-09 15:55:17 +1000

    adding some comment

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hurry_up_henry/repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hurry_up_henry/simple_bloc_observer.dart';
 
+//some change
 void main() {
   Bloc.observer = SimpleBlocObserver();
   runApp(App(repository: Repository()));
