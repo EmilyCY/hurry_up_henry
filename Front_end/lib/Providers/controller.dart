cc3303ab06fd56fa37586f4d619618be2270b7f5
Jordan <jordanremzij@gmail.com>
2021-09-23 15:35:32 +1000

    merge fixes

--- Front_end/lib/Providers/controller.dart
+++ Front_end/lib/Providers/controller.dart
@@ -11,34 +11,26 @@ final winSFXpath = "assets/win.wav";
 final loseSFXpath = "assets/lose.wav";
 final moveSFXpath = "assets/move.wav";
 
+enum ActionType { Delete, Run, CarSwitch, Up, Down, Left, Right }
+
 class Controller with ChangeNotifier {
   int currentPosition = 0;
   int goalPosition = 0;
   static Random randomNum = new Random();
   int startPosition = 0;
   List<Move> moves = [];
-  List<int> leftBoundary = [];
-  List<int> rightBoundary = [];
-  List<int> upperBoundary = [];
-  List<int> lowerBoundary = [];
 
   Controller() {
-    //this.startPosition = randomNum.nextInt(99);
-    //this.currentPosition = this.startPosition;
     currentPosition = randomNum.nextInt(99);
     newGame();
-    this.leftBoundary = getLeftBoundary();
-    this.upperBoundary = getUpperBoundary();
-    this.rightBoundary = getRightBoundary();
-    this.lowerBoundary = getLowerBoundary();
   }
 
   void getCurrentPosition() async {
     for (Move move in moves) {
       await Future.delayed(Duration(milliseconds: 500));
 
-      if (!isOutOfBounds(this.currentPosition, move.direction)) {
-        this.currentPosition += move.positionChange;
+      if (!isOutOfBounds(currentPosition, move.direction)) {
+        currentPosition += move.positionChange;
         player.play(moveSFXpath);
       } else {
         lose();
@@ -50,7 +42,26 @@ class Controller with ChangeNotifier {
     }
     this.moves = [];
     winCondition();
-    //fail state?
+  }
+
+  void newGame() {
+    newGoal();
+    moves = [];
+  }
+
+  void newGoal() {
+    goalPosition = randomNum.nextInt(99);
+  }
+
+  void makeMove(String direction) {
+    Move move = new Move(direction);
+    this.moves.add(move);
+    notifyListeners();
+  }
+
+  void deleteMove() {
+    moves.removeLast();
+    notifyListeners();
   }
 
   void winCondition() {
@@ -69,115 +80,37 @@ class Controller with ChangeNotifier {
     notifyListeners();
   }
 
-  void run() {}
-
-  void newGoal() {
-    // generate random goal pos that is at least 2 sq away from current pos
-    goalPosition = randomNum.nextInt(99);
-    //List<int> noGoalZone = [];
-  }
-
-  void newGame() {
-    newGoal();
-    moves = [];
-  }
-
-  void makeMove(String direction) {
-    /*
-    List<int> boundary = [];
-
+  bool isOutOfBounds(int currentPosition, String direction) {
     switch (direction) {
       case "left":
         {
-          boundary = this.leftBoundary;
+          if (currentPosition % sqrt(GRID_NUM) != 0) {
+            return false;
+          }
         }
         break;
       case "right":
         {
-          boundary = this.rightBoundary;
+          if (currentPosition % sqrt(GRID_NUM) != sqrt(GRID_NUM) - 1) {
+            return false;
+          }
         }
         break;
       case "up":
         {
-          boundary = this.upperBoundary;
+          if (currentPosition - sqrt(GRID_NUM) >= 0) {
+            return false;
+          }
         }
         break;
       case "down":
         {
-          boundary = this.lowerBoundary;
+          if (currentPosition + sqrt(GRID_NUM) <= GRID_NUM) {
+            return false;
+          }
         }
         break;
     }
-    if (boundary.every((n) => n != this.currentPosition)) {
-      Move move = new Move(direction);
-      this.moves.add(move);
-      notifyListeners();
-    }
-    */
-    Move move = new Move(direction);
-    this.moves.add(move);
-    notifyListeners();
+    return true;
   }
-
-  List<int> getLeftBoundary() {
-    for (int i = 0; i < sqrt(GRID_NUM); i++) {
-      this.leftBoundary.add(sqrt(GRID_NUM).toInt() * i);
-    }
-    return this.leftBoundary;
-  }
-
-  List<int> getRightBoundary() {
-    for (int i = 0; i < sqrt(GRID_NUM); i++) {
-      this.rightBoundary.add(sqrt(GRID_NUM).toInt() * (i + 1) - 1);
-    }
-    return this.leftBoundary;
-  }
-
-  List<int> getUpperBoundary() {
-    for (int i = 0; i < sqrt(GRID_NUM) - 1; i++) {
-      this.upperBoundary.add(i);
-    }
-    return this.upperBoundary;
-  }
-
-  List<int> getLowerBoundary() {
-    for (int i = 0; i < sqrt(GRID_NUM) - 1; i++) {
-      this.lowerBoundary.add(GRID_NUM - 1 - i);
-    }
-    return this.lowerBoundary;
-  }
-}
-
-bool isOutOfBounds(int currentPosition, String direction) {
-  switch (direction) {
-    case "left":
-      {
-        if (currentPosition % sqrt(GRID_NUM) != 0) {
-          return false;
-        }
-      }
-      break;
-    case "right":
-      {
-        if (currentPosition % sqrt(GRID_NUM) != sqrt(GRID_NUM) - 1) {
-          return false;
-        }
-      }
-      break;
-    case "up":
-      {
-        if (currentPosition - sqrt(GRID_NUM) >= 0) {
-          return false;
-        }
-      }
-      break;
-    case "down":
-      {
-        if (currentPosition + sqrt(GRID_NUM) <= GRID_NUM) {
-          return false;
-        }
-      }
-      break;
-  }
-  return true;
 }
